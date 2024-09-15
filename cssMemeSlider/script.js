603cc7248543503b4d693921f9fa3661e571e0a4
Aliaksandra <a.sinitsyna07@gmail.com>
2024-09-16 00:14:02 +0200

    refactor: apply prettier (Mon, Sep 16, 2024 12:14:02 AM)

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,22 +1,21 @@
-const slider = document.querySelector('.slider');
-const sliderItems = document.querySelectorAll('.slider-item');
-const dots = document.querySelectorAll('.dot');
+const slider = document.querySelector(".slider");
+const sliderItems = document.querySelectorAll(".slider-item");
+const dots = document.querySelectorAll(".dot");
 
 let currentIndex = 0;
 
 function updateSlider() {
   const offset = -currentIndex * 100;
-  sliderItems.forEach(item => {
+  sliderItems.forEach((item) => {
     item.style.transform = `translateX(${offset}%)`;
   });
-  
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[currentIndex].classList.add('active');
+
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[currentIndex].classList.add("active");
 }
 
-
 dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
+  dot.addEventListener("click", () => {
     currentIndex = index;
     updateSlider();
   });
